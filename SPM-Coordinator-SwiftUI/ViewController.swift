44865e7584214e8c6420ab18f2df7242f49f2502
Bruno Chen <brunochenchihying@gmail.com>
2021-03-20 19:12:23 -0300

    first commit

---
+++ SPM-Coordinator-SwiftUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPM-Coordinator-SwiftUI
+//
+//  Created by bruno chen chih ying on 20/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
